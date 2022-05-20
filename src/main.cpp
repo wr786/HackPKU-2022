21f3aa8e00d68b5fd503edeecccd9ddf49551de1
yueyue-1999 <64481850+yueyue-1999@users.noreply.github.com>
2022-05-21 00:52:59 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,7 @@ int main()
     scenes[SCENE_MAIN] = new SceneMain();
     scenes[SCENE_SELECT] = new SceneSelect();
     scenes[SCENE_PLAY] = new ScenePlay();
+    scenes[SCENE_SCORE] = new SceneScore();
     SceneBase* g = scenes[sceneType];
 
     InitWindow(g->screenWidth, g->screenHeight, "Unnamed");
