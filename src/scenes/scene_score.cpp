07c1af8ecc0ba1e51c9d7a2e569adabfe1d332df
yueyue-1999 <64481850+yueyue-1999@users.noreply.github.com>
2022-05-21 17:56:02 +0800

    Update scene_score.cpp

--- src/scenes/scene_score.cpp
+++ src/scenes/scene_score.cpp
@@ -16,12 +16,11 @@ private:
 
 public:
     void init() {
-        songName = "sand";
+        songName = "aria";
 
         font_caption = LoadFontEx("../resource/font/bb2180.ttf", 96, 0, 0);
         string path = IMAGE_FOLDER + songName + ".png";
         texture_background = LoadTexture("../resource/image/bg_scenescore.png");
-        score = 0;
         SetTargetFPS(60);
     }
 
@@ -53,47 +52,21 @@ public:
             DrawTriangle({595, 450}, {700, 555}, {805, 450}, Fade(BLACK, 0.9f));
             DrawTriangle({615, 450}, {785, 450}, {700, 365}, Fade(WHITE, 0.3f));
             DrawTriangle({615, 450}, {700, 535}, {785, 450}, Fade(WHITE, 0.3f));
-            if(score <= 5000000) {
+            if(score <= 20) {
                 DrawTextEx(font_caption, TextFormat("D"), {680, 420}, 80, 0, GREEN);
-            } else if(score <= 7000000) {
+            } else if(score <= 40) {
                 DrawTextEx(font_caption, TextFormat("C"), {680, 420}, 80, 0, RED);
-            } else if(score <= 8000000) {
+            } else if(score <= 60) {
                 DrawTextEx(font_caption, TextFormat("B"), {680, 420}, 150, 0, YELLOW);
-            } else if(score <= 9000000) {
+            } else if(score <= 80) {
                 DrawTextEx(font_caption, TextFormat("A"), {680, 420}, 150, 0, ORANGE);
-            } else if(score <= 9500000) {
+            } else if(score <= 100) {
                 DrawTextEx(font_caption, TextFormat("S"), {680, 420}, 150, 0, PURPLE);
-            } else if(score < 10000000) {
+            } else if(score < 120) {
                 DrawTextEx(font_caption, TextFormat("V"), {680, 420}, 150, 0, BLUE);
-            } else if(score == 10000000) {
+            } else if(score >= 140) {
                 DrawTextEx(font_caption, TextFormat("P"), {680, 420}, 150, 0, PINK);
             }
-
-            // // // 画ACC
-            // // DrawTextEx(font_caption, TextFormat("ACC:%.2f%%", scoreboard.get_acc()), {207, 433}, 30, 0, WHITE);
-            // // // 画Combo //todo full combo特殊字幕
-            // // DrawTextEx(font_caption, TextFormat("Max Combo: %d", scoreboard.maxCombo), {1060, 433}, 40, 0, WHITE);
-            // // // 画各note个数
-            // // DrawTextEx(font_caption, TextFormat("PURE"), {700, 610}, 30, 0, PINK);
-            // // DrawTextEx(font_caption, TextFormat("FAR"), {900, 610}, 30, 0, ORANGE);
-            // // DrawTextEx(font_caption, TextFormat("LOST"), {1100, 610}, 30, 0, GRAY);
-            // // DrawTextEx(font_caption, TextFormat("%3d", scoreboard.pure), {700, 565}, 40, 0, WHITE);
-            // // DrawTextEx(font_caption, TextFormat("%3d", scoreboard.far), {888, 565}, 40, 0, WHITE);
-            // // DrawTextEx(font_caption, TextFormat("%3d", scoreboard.lost), {1095, 565}, 40, 0, WHITE);
-
-            // // // 画历史分
-            // // if(!createScore) {  // 如果
-            // //     if(scoreboard.get_score() >= curSelectedHighscore)
-            // //         DrawTextEx(font_caption, TextFormat("NEW BEST"), {660, 280}, 40, 0, GREEN);
-            // //     DrawTextEx(font_caption, TextFormat("%08d",curSelectedHighscore), {960, 280}, 40, 0, WHITE);
-            // //     string fmt = scoreboard.get_score() >= curSelectedHighscore? "+%8d" : "-%8d";
-            // //     DrawTextEx(font_caption, TextFormat(fmt.c_str(), abs(scoreboard.get_score() - curSelectedHighscore)), {1200, 280}, 40, 0, WHITE);
-            // // }
-            // // 画字幕
-            // // if(createScore) {
-            // DrawTextEx(font_caption, TextFormat("CREATE MODE CREATE MODE CREATE MODE CREATE MODE CREATE MODE"), {0, 270}, 60, 0, PINK);
-            // DrawTextEx(font_caption, TextFormat("ATE MODE CREATE MODE CREATE MODE CREATE MODE CREATE MODE"), {0, 570}, 60, 0, PINK);
-            // // }
 
         EndDrawing();
 
